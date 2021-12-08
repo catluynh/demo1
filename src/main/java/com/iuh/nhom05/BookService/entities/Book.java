fcc4c9de5425e8249964dcddf423992a61e2cffd
catluynh <catluynh99@gmail.com>
2021-12-08 09:37:02 +0700

    commit

---
+++ src/main/java/com/iuh/nhom05/BookService/entities/Book.java
@@ -0,0 +1,27 @@
+package com.iuh.nhom05.BookService.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Book {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String imageUrl;
+    private String title;
+    private int authorId;
+    private double price;
+    private int publishedYear;
+}
